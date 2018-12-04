8a565afd694d853594bfe04ba88fff713c04c4bc
Jullia Sannikova <julliab2014@yandex.ru>
2018-12-04 23:18:14 +0300

    Добавление схемы базы данных

--- src/main/java/repository/IssureRepository.java
+++
@@ -1,7 +0,0 @@
-package repository;
-
-import entity.Issure;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IssureRepository extends JpaRepository<Issure, Long> {
-}
--- src/main/java/repository/ProjectRepository.java
+++
@@ -1,7 +0,0 @@
-package repository;
-
-import entity.Project;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProjectRepository extends JpaRepository<Project, Long> {
-}

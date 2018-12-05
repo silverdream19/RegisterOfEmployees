7524dbdbd0fda772b27cd6425745184d4eee1a59
Jullia Sannikova <julliab2014@yandex.ru>
2018-12-05 21:19:00 +0300

    Исправлены связи базы данных

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,7 +2,7 @@ INSERT INTO Organization VALUES (1, "Березка", "ООО Березка", "
 INSERT INTO Organization VALUES (2, "Ромашка", "ООО Ромашка", "72345", "74321", "Тверская дом 5", "5455454", false);
 INSERT INTO Office VALUES (1, 1, "офис 2", "Брянская улица дом 7", "98765", true);
 INSERT INTO Office VALUES (2, 1, "офис 3", "Брянская улица дом 7", "6766677", false);
-INSERT INTO Employee VALUES (1, 1, "Иван", "Иванов", "Петрович","Менеджер",1,"01.01.2007",1, true);
-INSERT INTO Employee VALUES (1, 1, "Максим", "Жуков", "Николаевич","Менеджер",1,"01.01.2008",1, true);
-INSERT INTO Document VALUES (1, "Паспорт РФ", "632");
+INSERT INTO Employee VALUES (1, 1, "Иван", "Иванов", "Петрович","Менеджер",1, true);
+INSERT INTO Employee VALUES (1, 1, "Максим", "Жуков", "Николаевич","Менеджер",1, true);
+INSERT INTO Document VALUES (1, "Паспорт РФ","01.01.2007", "632");
 INSERT INTO Citizenship VALUES (1, "Российская Федерация", "21");
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -12,13 +12,10 @@ COMMENT ON TABLE Organization IS 'Организация';
 
 CREATE TABLE IF NOT EXISTS Employee (
     id              INTEGER              COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT ,
-    office_id       INTEGER     NOT NULL COMMENT 'Офис',
     first_name      VARCHAR(75) NOT NULL COMMENT 'Имя',
     second_name     VARCHAR(75) NOT NULL COMMENT 'Фамилия',
     middle_name     VARCHAR(75) NOT NULL COMMENT 'Отчество',
     employee_position   VARCHAR(20) NOT NULL COMMENT 'Должность',
-    doc_type_id     INTEGER     NOT NULL COMMENT 'Тип документа',
-    doc_date        DATE                 COMMENT 'Дата документа',
     citizenship_id  INTEGER     NOT NULL COMMENT 'Гражданство',
     is_identified   BOOLEAN
 );
@@ -26,8 +23,10 @@ COMMENT ON TABLE Employee IS 'Сотрудник';
 
 CREATE TABLE IF NOT EXISTS Office (
     id         INTEGER              COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT ,
+    user_id    INTEGER     NOT NULL COMMENT 'Пользователь',
     org_id     INTEGER     NOT NULL COMMENT 'Организация',
     name       VARCHAR(50) NOT NULL COMMENT 'Наименование',
+    id_doc     INTEGER              COMMENT 'Тип документа'
     address    VARCHAR(50) NOT NULL COMMENT 'Адрес',
     phone      VARCHAR(20)          COMMENT 'Телефон',
     is_active  BOOLEAN
@@ -36,7 +35,9 @@ COMMENT ON TABLE Office IS 'Офис';
 
 CREATE TABLE IF NOT EXISTS Document (
     id         INTEGER                     COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT ,
+    user_id    INTEGER                     COMMENT 'Пользователь',
     name       VARCHAR(50) UNIQUE NOT NULL COMMENT 'Наименование документа',
+    doc_date   DATE                        COMMENT 'Дата документа',
     code       VARCHAR(2)  UNIQUE NOT NULL COMMENT 'Код документа'
 );
 COMMENT ON TABLE Office IS 'Тип документа';
@@ -47,6 +48,7 @@ CREATE TABLE IF NOT EXISTS Citizenship (
     code    VARCHAR(2)  UNIQUE NOT NULL COMMENT 'Код страны'
 );
 COMMENT ON TABLE Office IS 'Гражданство';
+
 
 CREATE INDEX IX_Organization_Id ON Organization (id);
 CREATE INDEX IX_Organization_Full_Name ON Organization (full_name);
@@ -59,10 +61,11 @@ CREATE INDEX IX_Employee_First_Name ON Employee (first_name);
 CREATE INDEX IX_Employee_Second_Name ON Employee (second_name);
 CREATE INDEX IX_Employee_Middle_Name ON Employee (middle_name);
 CREATE INDEX IX_Employee_Employee_Position ON Employee (employee_position);
-ALTER TABLE Employee ADD FOREIGN KEY (doc_type_id) REFERENCES Document(id);
 ALTER TABLE Employee ADD FOREIGN KEY (citizenship_id) REFERENCES Citizenship(id);
-ALTER TABLE Employee ADD FOREIGN KEY (office_id) REFERENCES Office(id);
+ALTER TABLE Docunent ADD FOREIGN KEY (user_id) REFERENCES Employee(id);
 
 CREATE INDEX IX_Office_Id ON Office (id);
 CREATE INDEX IX_Office_Name ON Office (name);
 ALTER TABLE Office ADD FOREIGN KEY (org_id) REFERENCES Organization(id);
+ALTER TABLE Office ADD FOREIGN KEY (user_id) REFERENCES "Employee(id);
+

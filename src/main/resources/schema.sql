e0ac6229cf39965cb56449f376f1e9ae258556cb
Jullia Sannikova <julliab2014@yandex.ru>
2018-12-05 22:18:54 +0300

    Исправлены связи базы данных

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -39,14 +39,14 @@ CREATE TABLE IF NOT EXISTS Document (
     doc_date   DATE                        COMMENT 'Дата документа',
     code       VARCHAR(2)  UNIQUE NOT NULL COMMENT 'Код документа'
 );
-COMMENT ON TABLE Office IS 'Тип документа';
+COMMENT ON TABLE Document IS 'Тип документа';
 
 CREATE TABLE IF NOT EXISTS Citizenship (
     id      INTEGER                     COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT ,
     name    VARCHAR(50) UNIQUE NOT NULL COMMENT 'Гражданство',
     code    VARCHAR(2)  UNIQUE NOT NULL COMMENT 'Код страны'
 );
-COMMENT ON TABLE Office IS 'Гражданство';
+COMMENT ON TABLE Citizenship IS 'Гражданство';
 
 
 CREATE INDEX IX_Organization_Id ON Organization (id);
@@ -61,10 +61,10 @@ CREATE INDEX IX_Employee_Second_Name ON Employee (second_name);
 CREATE INDEX IX_Employee_Middle_Name ON Employee (middle_name);
 CREATE INDEX IX_Employee_Employee_Position ON Employee (employee_position);
 ALTER TABLE Employee ADD FOREIGN KEY (citizenship_id) REFERENCES Citizenship(id);
-ALTER TABLE Docunent ADD FOREIGN KEY (user_id) REFERENCES Employee(id);
+ALTER TABLE Document ADD FOREIGN KEY (user_id) REFERENCES Employee(id);
 
 CREATE INDEX IX_Office_Id ON Office (id);
 CREATE INDEX IX_Office_Name ON Office (name);
 ALTER TABLE Office ADD FOREIGN KEY (org_id) REFERENCES Organization(id);
-ALTER TABLE Office ADD FOREIGN KEY (user_id) REFERENCES "Employee(id);
+ALTER TABLE Office ADD FOREIGN KEY (user_id) REFERENCES Employee(id);
 

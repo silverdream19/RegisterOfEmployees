07a6379b989a889f9aff2b8cabd5c3f6b32b115e
Jullia Sannikova <julliab2014@yandex.ru>
2018-12-06 23:24:06 +0300

    обавлена связь many-to-many

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,12 @@
 INSERT INTO Organization VALUES (1, "Березка", "ООО Березка", "12345", "54321", "Улица Ленина дом 1", "2122121", true);
 INSERT INTO Organization VALUES (2, "Ромашка", "ООО Ромашка", "72345", "74321", "Тверская дом 5", "5455454", false);
-INSERT INTO Office VALUES (1, 1, 1, "офис 2", "Брянская улица дом 7", "98765", true);
-INSERT INTO Office VALUES (2, 2, 1, "офис 3", "Брянская улица дом 7", "6766677", false);
+INSERT INTO Office VALUES (1, 1, "офис 2", "Брянская улица дом 7", "98765", true);
+INSERT INTO Office VALUES (2, 2, "офис 3", "Брянская улица дом 7", "6766677", false);
 INSERT INTO Employee VALUES (1, "Иван", "Иванов", "Петрович","Менеджер",1, true);
 INSERT INTO Employee VALUES (2, "Максим", "Жуков", "Николаевич","Менеджер",1, true);
-INSERT INTO Document VALUES (1,1, "Паспорт РФ","01.01.2007", "632");
+INSERT INTO Document VALUES (1, 1, "Паспорт РФ","01.01.2007", "632");
+INSERT INTO Document VALUES (2, 1, "Водительское удостоверение","01.07.2012", "453");
 INSERT INTO Citizenship VALUES (1, "Российская Федерация", "21");
+INSERT INTO Employee_Office VALUES (1, 1);
+INSERT INTO Employee_Office VALUES (1, 2);
+INSERT INTO Employee_Office VALUES (2, 1);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,70 +1,1 @@
-CREATE TABLE IF NOT EXISTS Organization (
-    id         INTEGER                     COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT,
-    name       VARCHAR(150)       NOT NULL COMMENT 'Наименование',
-    full_name  VARCHAR(200)       NOT NULL COMMENT 'Полное наименование',
-    inn        VARCHAR(20) UNIQUE NOT NULL COMMENT 'ИНН',
-    kpp        VARCHAR(20) UNIQUE NOT NULL COMMENT 'КПП',
-    address    VARCHAR(150)       NOT NULL COMMENT 'Адрес',
-    phone      VARCHAR(20)                 COMMENT 'Телефон',
-    is_active  BOOLEAN
-);
-COMMENT ON TABLE Organization IS 'Организация';
-
-CREATE TABLE IF NOT EXISTS Employee (
-    id              INTEGER              COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT ,
-    first_name      VARCHAR(75) NOT NULL COMMENT 'Имя',
-    second_name     VARCHAR(75) NOT NULL COMMENT 'Фамилия',
-    middle_name     VARCHAR(75) NOT NULL COMMENT 'Отчество',
-    employee_position   VARCHAR(20) NOT NULL COMMENT 'Должность',
-    citizenship_id  INTEGER     NOT NULL COMMENT 'Гражданство',
-    is_identified   BOOLEAN
-);
-COMMENT ON TABLE Employee IS 'Сотрудник';
-
-CREATE TABLE IF NOT EXISTS Office (
-    id         INTEGER              COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT ,
-    user_id    INTEGER     NOT NULL COMMENT 'Пользователь',
-    org_id     INTEGER     NOT NULL COMMENT 'Организация',
-    name       VARCHAR(50) NOT NULL COMMENT 'Наименование',
-    address    VARCHAR(50) NOT NULL COMMENT 'Адрес',
-    phone      VARCHAR(20)          COMMENT 'Телефон',
-    is_active  BOOLEAN
-);
-COMMENT ON TABLE Office IS 'Офис';
-
-CREATE TABLE IF NOT EXISTS Document (
-    id         INTEGER                     COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT ,
-    user_id    INTEGER                     COMMENT 'Пользователь',
-    name       VARCHAR(50) UNIQUE NOT NULL COMMENT 'Наименование документа',
-    doc_date   DATE                        COMMENT 'Дата документа',
-    code       VARCHAR(2)  UNIQUE NOT NULL COMMENT 'Код документа'
-);
-COMMENT ON TABLE Document IS 'Тип документа';
-
-CREATE TABLE IF NOT EXISTS Citizenship (
-    id      INTEGER                     COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT ,
-    name    VARCHAR(50) UNIQUE NOT NULL COMMENT 'Гражданство',
-    code    VARCHAR(2)  UNIQUE NOT NULL COMMENT 'Код страны'
-);
-COMMENT ON TABLE Citizenship IS 'Гражданство';
-
-
-CREATE INDEX IX_Organization_Id ON Organization (id);
-CREATE INDEX IX_Organization_Full_Name ON Organization (full_name);
-CREATE INDEX UX_Organization_Inn ON Organization (inn);
-CREATE INDEX UX_Organization_Kpp ON Organization (kpp);
-
-
-CREATE INDEX IX_Employee_Id ON Employee (id);
-CREATE INDEX IX_Employee_First_Name ON Employee (first_name);
-CREATE INDEX IX_Employee_Second_Name ON Employee (second_name);
-CREATE INDEX IX_Employee_Middle_Name ON Employee (middle_name);
-CREATE INDEX IX_Employee_Employee_Position ON Employee (employee_position);
-ALTER TABLE Employee ADD FOREIGN KEY (citizenship_id) REFERENCES Citizenship(id);
-ALTER TABLE Document ADD FOREIGN KEY (user_id) REFERENCES Employee(id);
-
-CREATE INDEX IX_Office_Id ON Office (id);
-CREATE INDEX IX_Office_Name ON Office (name);
-ALTER TABLE Office ADD FOREIGN KEY (org_id) REFERENCES Organization(id);
-ALTER TABLE Office ADD FOREIGN KEY (user_id) REFERENCES Employee(id);
-
+CREATE TABLE IF NOT EXISTS Organization (    id         INTEGER                     COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT,    name       VARCHAR(150)       NOT NULL COMMENT 'Наименование',    full_name  VARCHAR(200)       NOT NULL COMMENT 'Полное наименование',    inn        VARCHAR(20) UNIQUE NOT NULL COMMENT 'ИНН',    kpp        VARCHAR(20) UNIQUE NOT NULL COMMENT 'КПП',    address    VARCHAR(150)       NOT NULL COMMENT 'Адрес',    phone      VARCHAR(20)                 COMMENT 'Телефон',    is_active  BOOLEAN);COMMENT ON TABLE Organization IS 'Организация';CREATE TABLE IF NOT EXISTS Employee (    id              INTEGER              COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT ,    first_name      VARCHAR(75) NOT NULL COMMENT 'Имя',    second_name     VARCHAR(75) NOT NULL COMMENT 'Фамилия',    middle_name     VARCHAR(75) NOT NULL COMMENT 'Отчество',    employee_position   VARCHAR(20) NOT NULL COMMENT 'Должность',    citizenship_id  INTEGER     NOT NULL COMMENT 'Гражданство',    is_identified   BOOLEAN);COMMENT ON TABLE Employee IS 'Сотрудник';CREATE TABLE IF NOT EXISTS Office (    id         INTEGER              COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT ,    org_id     INTEGER     NOT NULL COMMENT 'Организация',    name       VARCHAR(50) NOT NULL COMMENT 'Наименование',    address    VARCHAR(50) NOT NULL COMMENT 'Адрес',    phone      VARCHAR(20)          COMMENT 'Телефон',    is_active  BOOLEAN);COMMENT ON TABLE Office IS 'Офис';CREATE TABLE IF NOT EXISTS Document (    id         INTEGER                     COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT ,    user_id    INTEGER                     COMMENT 'Пользователь',    name       VARCHAR(50) UNIQUE NOT NULL COMMENT 'Наименование документа',    doc_date   DATE                        COMMENT 'Дата документа',    code       VARCHAR(2)  UNIQUE NOT NULL COMMENT 'Код документа');COMMENT ON TABLE Document IS 'Тип документа';CREATE TABLE IF NOT EXISTS Citizenship (    id      INTEGER                     COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT ,    name    VARCHAR(50) UNIQUE NOT NULL COMMENT 'Гражданство',    code    VARCHAR(2)  UNIQUE NOT NULL COMMENT 'Код страны');COMMENT ON TABLE Citizenship IS 'Гражданство';CREATE TABLE IF NOT EXISTS Employee_Office (    user_id   INTEGER COMMENT 'Уникальный идентификатор пользователя',    office_id INTEGER COMMENT 'Уникальный идентификатор офиса'    PRIMARY KEY (user_id, office_id));COMMENT ON TABLE Citizenship IS 'join таблица для соединения пользователя и офиса';CREATE INDEX IX_Organization_Id ON Organization (id);CREATE INDEX IX_Organization_Full_Name ON Organization (full_name);CREATE INDEX UX_Organization_Inn ON Organization (inn);CREATE INDEX UX_Organization_Kpp ON Organization (kpp);CREATE INDEX IX_Employee_Id ON Employee (id);CREATE INDEX IX_Employee_First_Name ON Employee (first_name);CREATE INDEX IX_Employee_Second_Name ON Employee (second_name);CREATE INDEX IX_Employee_Middle_Name ON Employee (middle_name);CREATE INDEX IX_Employee_Employee_Position ON Employee (employee_position);CREATE INDEX IX_Employee_Citizenship_Id ON Employee (citizenship_id);ALTER TABLE Employee ADD FOREIGN KEY (citizenship_id) REFERENCES Citizenship(id);CREATE INDEX IX_Document_User_Id ON Document (user_id);ALTER TABLE Document ADD FOREIGN KEY (user_id) REFERENCES Employee(id);CREATE INDEX IX_Office_Id ON Office (id);CREATE INDEX IX_Office_Name ON Office (name);CREATE INDEX IX_Office_Org_Id ON Office (org_id);ALTER TABLE Office ADD FOREIGN KEY (org_id) REFERENCES Organization(id);CREATE INDEX IX_Employee_Office_Id ON Employee_Office (office_id);ALTER TABLE Employee_Office ADD FOREIGN KEY (office_id) REFERENCES Office(id);CREATE INDEX IX_Office_Employee_Id ON Employee_Office (user_id);ALTER TABLE Employee_Office ADD FOREIGN KEY (user_id) REFERENCES Employee(id);
